f1ec06f9ecb97ee58eecd91f32522ccb7436512b
fjakrapan <jakrapanworkshop64@gmail.com>
2024-07-30 17:15:04 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+}from "react-router-dom";
+import SingIn from './pages/backoffice/SingIn';
+import Home from './pages/backoffice/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SingIn />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <RouterProvider router={router} />
+);
